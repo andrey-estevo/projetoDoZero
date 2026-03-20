2780023a81348c65a55ea76b998511847b996860
ANDREY SANTOS ESTEVO <andrey.sestevo@senacsp.edu.br>
2026-03-19 21:58:56 -0300

    projeto funcionando

---
+++ src/app/Profile.tsx
@@ -0,0 +1,10 @@
+import { View, Image } from "react-native";
+export function Profile (){
+    return(
+        <View>
+            <Image
+            source={require("@/assets/banner.png")}
+            className="w-full h-52"/>
+        </View>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+//import { colors } from "@/styles/colors"
+const { colors } = require ("./src/styles/colors");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "App.tsx","./components/**/*.{ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+  
